526711fca1d3dc06d529607f5e72e6fda657dfbd
Ubuntu <ubuntu@ip-10-212-187-227.ec2.internal>
2013-03-22 18:37:16 +0000

    done

--- instazon/client/instazon.js
+++ instazon/client/instazon.js
@@ -1,46 +1,48 @@
 Results = new Meteor.Collection("results");
 
-Session.setDefault('user_id', Math.round((Math.random()*(5000000))));
+Session.set("user_id", "results");
 
-Deps.autorun(function() {
+var lastSearch = "";
+
+Meteor.autosubscribe(function() {
     Meteor.subscribe("results", Session.get('user_id'));
 });
 
 Template.results.results = function() {
-    var id = Session.get('user_id');
-    return Results.find({user_id: id});
+ return Results.find({user_id: Session.get('user_id')});
 };
 
 Template.results.events({
     'keyup input#appendedInputButton' : function(){
+        var keywords = document.getElementById("appendedInputButton").value;
+        // experimenting with delay so I don't search every time
+        // while(keywords !== document.getElementById("appendedInputButton").value)
+        //   keywords = document.getElementById("#appendedInputButton").value;
         var id = Session.get('user_id');
         var res = Results.find({user_id: id});
         if(res){
-          res.forEach(function (results) {
-            res.remove(this._id);
+          //remove previous results
+          Meteor.call('removeResults', id, function(err, results){
+            if(err)
+              console.log(err);
+            Session.set({user_id: ""});
           });
         }
-        var keywords = document.getElementById("appendedInputButton").value;
-        if(keywords!==""){
+        //only search if there are keys and they are different than before
+        if(keywords!=="" && keywords!==lastSearch){
+          lastSearch = keywords;
           Meteor.call('getResultsFromAPI', keywords, id, function(err, results) {
             if (err)
               console.log(err);
-        
-            Session.set('user_id', id);//Session.get(user_id));
+            Session.set('user_id', "results");
           });
-          //console.log("Back here");
-          var results = Results.find({user_id: id});
-          //console.log(results);
-//           for (var key in results) { 
-//     console.log(key + ": " + results[key]); 
-// }
-          console.log(results);
         }
     }
 });
 
-Template.item.events({
-  '.display' : function () {
-    Deps.flush();
-  }
+//adds in these html elements to the handlebar
+Handlebars.registerHelper('helper', function(){
+  return new Handlebars.SafeString(
+    "<td><img src=\""+this.image+"\" class=\"img\" alt=\"Product\"></td><td><div class=\"name\">"+this.name+"</div></td><td><div class=\"price\">"+this.price+"</div></td><td><a href=\""+this.link+"\" class=\"btn\">Buy Now!</a></td>"
+  );
 });
--- instazon/server/server.js
+++ instazon/server/server.js
@@ -1,3 +1,4 @@
+//gets path to node.js api
 var require = __meteor_bootstrap__.require;
 var path = require("path");
 var fs = require('fs');
@@ -28,7 +29,7 @@ Meteor.methods({
   getResultsFromAPI:function (keywords, user_id){
       var util = require('util'),
       OperationHelper = APAC.OperationHelper;
-      
+      //uses node.js api to get results
       var opHelper = new OperationHelper({
 	  awsId:     'AKIAJJEDNXWQXJW7XBQA',
 	  awsSecret: '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',
@@ -41,14 +42,18 @@ Meteor.methods({
 	  'ResponseGroup': 'Medium,ItemAttributes'
       }, function(error, results) {
 	  if (error) { console.log('Error: ' + error + "\n") }
+	  var root = results.ItemSearchResponse.Items[0];
 	  for(var i=0; i<5; i++){
-	      var image = results.ItemSearchResponse.Items[0].Item[i].ImageSets[0].ImageSet[0].SmallImage[0].URL;
-	      var title = results.ItemSearchResponse.Items[0].Item[i].ItemAttributes[0].Title;
-	      var URL = results.ItemSearchResponse.Items[0].Item[i].DetailPageURL;
-	      var cost = results.ItemSearchResponse.Items[0].Item[i].OfferSummary[0].LowestNewPrice[0].FormattedPrice;
+	      var image = root.Item[i].ImageSets[0].ImageSet[0].SmallImage[0].URL;
+	      var title = root.Item[i].ItemAttributes[0].Title;
+	      var URL = root.Item[i].DetailPageURL;
+	      var cost = "N/A";
+	      if(root.Item[i].hasOwnProperty('OfferSummary')&&root.Item[i].OfferSummary[0].hasOwnProperty('LowestNewPrice'))
+		  cost = root.Item[i].OfferSummary[0].LowestNewPrice[0].FormattedPrice;
 	      Fiber(function(){
+		  //inserts the top five results
 		  Results.insert({
-		      id: user_id,
+		      user_id: user_id,
 		      image: image,
 		      price: cost,
 		      name: title,
@@ -56,8 +61,11 @@ Meteor.methods({
 		  });
 	      }).run();
 	  }
-	  return results;
       });
+  },
+    
+  removeResults: function(id){
+      Results.remove({user_id: id});
   }
 });
 
